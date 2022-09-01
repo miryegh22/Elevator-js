db27c1d93635fe5264e653acb95adda4698d95b1
miryegh22 <111610594+miryegh22@users.noreply.github.com>
2022-09-01 15:42:33 +0400

    Update elevator.js

--- elevator.js
+++ elevator.js
@@ -34,32 +34,46 @@ Elevator();
 
 function Move(id) {
   let movableLifts = ContainerLift.filter((lift) => lift.Checked == false);
-  
-    let distanceLiftmin = [];
-    let index = id.split("-");
-    let distance = movableLifts
-      .filter((lift) => (lift = lift.isMoving == false))
-      .map((lift) => Math.abs(index[1] - lift.LiftPosition));
-    let MinDistance = Math.min(...distance);
-    movableLifts.forEach((lift) => {
-      if (Math.abs(index[1] - lift.LiftPosition) == MinDistance) {
-        distanceLiftmin.push(lift);
-      }
+  let distanceLiftmin = [];
+  let index = id.split("-");
+  let distance = movableLifts
+    .filter((lift) => (lift = lift.isMoving == false))
+    .map((lift) => {
+      return Math.abs(index[1] - lift.LiftPosition);
     });
 
-    let contLift =
-      distanceLiftmin[Math.floor(Math.random() * distanceLiftmin.length)];
-    let count = contLift.LiftNumber;
-    document.querySelector(`#lift-box-${count}`).style.bottom = `${
-      index[1] * 3
-    }em`;
+  let MinDistance = Math.min(...distance);
+  if (
+    movableLifts[0].LiftPosition > 0 &&
+    movableLifts[1].LiftPosition > 0 &&
+    movableLifts[2].LiftPosition > 0
+  ) {
+    alert("Sorrt,try again ");
+  }
+  movableLifts.forEach((lift) => {
+    if (lift.LiftPosition > 0) {
+      lift.isMoving = true;
+    } else if (Math.abs(index[1] - lift.LiftPosition) == MinDistance) {
+      distanceLiftmin.push(lift);
+    }
+  });
 
-    ContainerLift[count].isMoving = true;
-    document.getElementById(`floor-${index[1]}`).disabled = true;
-    ContainerLift[count].LiftPosition = `${index[1]}`;
-    setTimeout(function () {
-      ContainerLift[count].isMoving = false;
-      document.getElementById(`floor-${index[1]}`).disabled = false;
-    }, 1000);
-  
+  let contLift =
+    distanceLiftmin[Math.floor(Math.random() * distanceLiftmin.length)];
+  let count = contLift.LiftNumber;
+  document.querySelector(`#lift-box-${count}`).style.bottom = `${
+    index[1] * 3
+  }em`;
+
+  ContainerLift[count].isMoving = true;
+  document.getElementById(`floor-${index[1]}`).disabled = true;
+  ContainerLift[count].LiftPosition = `${index[1]}`;
+  setTimeout(function () {
+    ContainerLift[count].isMoving = false;
+    document.getElementById(`floor-${index[1]}`).disabled = false;
+  }, 1000);
 }
+const liftBox = document.querySelectorAll(".lift-box");
+liftBox.forEach((box) => {
+  box.style.bottom = Math.floor(Math.random() * 909) + "px";
+});
